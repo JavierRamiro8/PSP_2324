ac642dc83931a820fe33da1439b12ca184a1f9b2
javierRamiro8 <jramiroc04@gmail.com>
2023-10-24 23:31:53 +0200

    Ejercicio 2 Acabado

--- lenguajes/c/RepasoExamen/ejercicio2.c
+++ lenguajes/c/RepasoExamen/ejercicio2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>  // Para rand()
+#include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h> // Para pid_t
+#include <sys/types.h>
 #include <sys/wait.h>
 
 #define MAXIMONUMERORANDOM 101
@@ -27,7 +27,6 @@ int main(int argc, char const *argv[])
         numerosAleatorios[i] = random;
     }
 
-
     hijo1 = fork();
 
     if (hijo1 == CERO)
@@ -45,16 +44,10 @@ int main(int argc, char const *argv[])
                 incrementador++;
             }
         }
-        for (int i = 0; i < incrementador; i++)
-        {
-            printf("Soy el hijo 1 y este es mi número par: %d\n", numerosPares[i]);
-        }
-        write(accionPares[1], &incrementador, sizeof(int));
+        write(accionPares[1], numerosPares, sizeof(numerosPares));
         close(accionPares[1]);
         exit(0);
     }
-
-
 
     hijo2 = fork();
     if (hijo2 == CERO)
@@ -72,19 +65,49 @@ int main(int argc, char const *argv[])
                 incrementador++;
             }
         }
-        for (int i = 0; i < incrementador; i++)
-        {
-            printf("Soy el hijo 2 y este es mi número impar: %d\n", numerosImpares[i]);
-        }
-        write(accionImpares[1], &incrementador, sizeof(int));
+        write(accionImpares[1], numerosImpares, sizeof(numerosImpares));
         close(accionImpares[1]);
         exit(0);
     }
 
-
-
     close(accionPares[1]);
     close(accionImpares[1]);
+
+    // Proceso padre
+    int numerosPares[20];
+    int numerosImpares[20];
+    int incrementadorHijo1 = 0;
+    int incrementadorHijo2 = 0;
+
+    read(accionPares[0], numerosPares, sizeof(numerosPares));
+    read(accionImpares[0], numerosImpares, sizeof(numerosImpares));
+
+    close(accionPares[0]);
+    close(accionImpares[0]);
+
+    for (int i = 0; i < 20; i++)
+    {
+        if (numerosPares[i] != 0)
+        {
+            incrementadorHijo1++;
+        }
+        if (numerosImpares[i] != 0)
+        {
+            incrementadorHijo2++;
+        }
+    }
+
+    printf("Números pares:\n");
+    for (int i = 0; i < incrementadorHijo1; i++)
+    {
+        printf("Soy el hijo 1 y este es mi número par: %d\n", numerosPares[i]);
+    }
+
+    printf("Números impares:\n");
+    for (int i = 0; i < incrementadorHijo2; i++)
+    {
+        printf("Soy el hijo 2 y este es mi número impar: %d\n", numerosImpares[i]);
+    }
 
     // Esperar a que los hijos terminen
     wait(NULL);
@@ -92,5 +115,4 @@ int main(int argc, char const *argv[])
 
     return 0;
 }
-
 
