142c5fd95ed11a212100a472eedccc3cc4c1323b
JavierRamiro8 <jramiroc04@gmail.com>
2023-11-23 21:38:29 +0100

    Carrera Terminada

--- lenguajes/Java/carrera/principal.java
+++ lenguajes/Java/carrera/principal.java
@@ -5,7 +5,7 @@ public class principal {
     private static int KMRECORRERMAX = 100;
     private static int CORREDORESMAX = 10;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         /*
          * int dorsal=10;
          * int dorsal2=101;
@@ -26,18 +26,19 @@ public class principal {
          */
 
         Thread[] corredorThread = new Thread[CORREDORESMAX];
-
         corredor[] corredores = new corredor[CORREDORESMAX];
-
+        Thread salida = new Thread();
         for (int i = 0; i < CORREDORESMAX; i++) {
-            corredores[i] = new corredor(KMRECORRERMAX, DORSAL+i);
+            corredores[i] = new corredor(KMRECORRERMAX, DORSAL + i);
             corredorThread[i] = new Thread(corredores[i]);
         }
-        for (int i = 0; i < CORREDORESMAX; i++) {
+        for(int i=0;i<corredores.length;i++){
             corredorThread[i].start();
         }
 
-
+        synchronized (salida) {
+            corredorThread.notifyAll();
+        }
         for (int i = 0; i < CORREDORESMAX; i++) {
             try {
                 corredorThread[i].join();
