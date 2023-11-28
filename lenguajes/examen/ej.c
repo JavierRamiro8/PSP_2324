fd3aa2315f2ac75245643b591bd985b793c367d0
JavierRamiro8 <jramiroc04@gmail.com>
2023-11-29 00:17:55 +0100

    Ejercicio base c

---
+++ lenguajes/examen/ej.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <strings.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <math.h>
+#include <time.h>
